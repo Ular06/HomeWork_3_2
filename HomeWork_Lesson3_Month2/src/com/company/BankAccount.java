40b49ea79b6f3543870a2293752b295d1c269945
Ular06 <drogetadb@gmail.com>
2022-10-14 21:43:00 +0600

    Initial commit

---
+++ HomeWork_Lesson3_Month2/src/com/company/BankAccount.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class BankAccount {
+    private double amount;
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void deposite(double sum) {
+        this.amount += sum;
+    }
+
+    public void withDraw(int sum) throws LimitException {
+        if (amount < sum) {
+            throw new LimitException("На вашем счёте недостаточно средств для проведение операции.\nНа вашем счёту: "
+                    + getAmount(), getAmount());
+        }
+        this.amount -= sum;
+    }
+}
